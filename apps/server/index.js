40ea6a13efd4f29bab1ee93b8bbc7329bcd0fac4
gagan <gagandeep20059@gmail.com>
2022-01-08 18:54:09 +0530

    added basic code for url creation and retrieval

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,6 +1,7 @@
 // Require the framework and instantiate it
 import Fastify from 'fastify';
 import { fastifyDynamo } from 'fastify-dynamo';
+import { createHash } from 'crypto';
 const fastify = Fastify({ logger: true });
 
 fastify.register(fastifyDynamo, {
@@ -13,10 +14,15 @@ fastify.register(fastifyDynamo, {
 });
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
+fastify.get('/:url', async (request, reply) => {
 	try {
-		const results = await fastify.dynamo.listTables({});
-		return results.TableNames;
+		getUrl(request.params.url, function (err, data) {
+			if (err) reply.status(500).send(err);
+			else {
+				if (data.Item) reply.send(data.Item.originalUrl.S);
+				else reply.send('Does not exist');
+			}
+		});
 	} catch (err) {
 		console.error(err);
 	}
@@ -35,9 +41,61 @@ fastify.route({
 		},
 	},
 	handler: async (request, reply) => {
-		return request.body;
+		let originalUrl = request.body.originalUrl;
+
+		// console.log(request)
+
+		/** Shortening using MD5 , will improve this in future */
+		let shortenedUrl = createHash('md5')
+			.update(originalUrl)
+			.digest('hex')
+			.slice(0, 6);
+
+		addUrl(originalUrl, shortenedUrl, function (err, data) {
+			if (err) {
+				reply.send(err);
+			} else {
+				console.log(data);
+				reply.send(shortenedUrl);
+			}
+		});
 	},
 });
+
+const addUrl = async (originalUrl, shortenedUrl, callback) => {
+	const params = {
+		Item: {
+			originalUrl: {
+				S: originalUrl,
+			},
+			shortenedUrl: {
+				S: shortenedUrl,
+			},
+		},
+		ReturnConsumedCapacity: 'TOTAL',
+		TableName: 'urls',
+	};
+
+	fastify.dynamo.putItem(params, function (err, data) {
+		callback(err, data);
+	});
+};
+
+const getUrl = async (originalUrl, callback) => {
+	console.log('oo', originalUrl);
+	const params = {
+		Key: {
+			shortenedUrl: {
+				S: originalUrl,
+			},
+		},
+		TableName: 'urls',
+	};
+
+	fastify.dynamo.getItem(params, function (err, data) {
+		callback(err, data);
+	});
+};
 
 // Run the server!
 const start = async () => {
--- packages/fastify-dynamo/index.js
+++ packages/fastify-dynamo/index.js
@@ -2,7 +2,7 @@ import fp from 'fastify-plugin';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
 
 function db(fastify, options = {}, next) {
-	const client = new DynamoDB(options);
+	const client = new DynamoDB({endpoint:'http://localhost:8000'});
 	if (!fastify.dynamo) {
 		fastify.decorate('dynamo', client);
 	}
