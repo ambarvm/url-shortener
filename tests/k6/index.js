e24b4cf91fb6c28b28c5c6dedc43553bba52c92f
KShivendu <shivendu@iitbhilai.ac.in>
2022-01-17 01:45:02 +0530

    feat(tests/k6): Add smoke and load tests

--- tests/k6/index.js
+++ tests/k6/index.js
@@ -1,13 +1,39 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
 
 const BASE_URL = "http://localhost:3000"
 
 export const options = {
     stages: [
-        { duration: '10s', target: 50 },
+        // Smoke tests (Tests to ensure basic functionality)
+        { duration: '3s', target: 3 }, // Only 3 users for the first 3 seconds.
+        
+        // Load tests (Normal day traffic)
+        { duration: '1m', target: 1000 }, // simulate ramp-up of traffic from 3 to 1000 users over the next 1 minute.
+        { duration: '2m', target: 1000 }, // stay at 100 users for 10 minutes
+        { duration: '3m', target: 0 }, // ramp-down to 0 users
+
+        // TODO: Stress tests (Finding the breaking point)
+        // Assesses the availability and stability of the system under extremely heavy load.
+
+        // TODO: Spike testing
     ],
+    thresholds: {
+        'http_req_duration': ['p(99)<1000'], // 99% of requests must complete below 1s
+        'url was shortened': ['p(99)<1000']  // 99% of requests must complete below 1s
+    },
 };
+
+export function handleSummary(data) {
+    return {
+        "/home/k6/summary.json": JSON.stringify(data),
+        "/home/k6/summary.html": htmlReport(data),
+        stdout: textSummary(data, { indent: " ", enableColors: true }),
+    };
+}
 
 export default function () {
     const shortUrlEndpoint = `${BASE_URL}/api/create`
