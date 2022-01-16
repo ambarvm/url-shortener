c8ee4c14e53987d8cbecc63b6714be12ac1680d1
gagan <gagandeep20059@gmail.com>
2022-01-16 14:32:30 +0530

    Added signup and api_key logic

--- apps/server/api/routes.js
+++ apps/server/api/routes.js
@@ -1,4 +1,7 @@
+import bcrypt from 'bcrypt';
+
 /** @type {import('fastify').FastifyPluginCallback} */
+
 const apiRoutes = async fastify => {
 	fastify.route({
 		method: 'POST',
@@ -6,18 +9,116 @@ const apiRoutes = async fastify => {
 		schema: {
 			body: {
 				type: 'object',
-				required: ['originalUrl'],
+				required: ['originalUrl', 'api_key'],
 				properties: {
 					originalUrl: { type: 'string' },
+					api_key: { type: 'string' },
 				},
 			},
 		},
-		handler: async request => {
-			let { originalUrl } = request.body;
-			let shortUrl = await fastify.db.createShortUrl(originalUrl);
-			return { shortUrl: `${request.hostname}/${shortUrl}` };
+		handler: async (request,reply) => {
+			let { originalUrl, api_key } = request.body;
+
+			if (await fastify.db.verifyApiKey(api_key)) {
+				let shortUrl = await fastify.db.createShortUrl(originalUrl, api_key);
+				return { shortUrl: `${request.hostname}/${shortUrl}` };
+			} else {
+				reply.status(403);
+				return 'Invalid Api key';
+			}
 		},
 	});
+
+	fastify.route({
+		method: 'POST',
+		url: '/register',
+		schema: {
+			body: {
+				type: 'object',
+				required: ['email', 'password'],
+				properties: {
+					email: { type: 'string', format: 'email' },
+					password: { type: 'string', minLength: 8 },
+				},
+			},
+		},
+		handler: async (req, reply) => {
+			try {
+				const { email, password } = req.body;
+
+				const oldUser = await fastify.db.getRegisteredUser(email);
+
+				if (oldUser !== false) {
+					reply.status(409);
+					return 'User already registered';
+				}
+				const encryptedUserPassword = await bcrypt.hash(password, 10);
+				const api_key = create_UUID();
+				const user = await fastify.db.createUser(
+					email.toLowerCase(),
+					encryptedUserPassword,
+					api_key,
+				);
+				return {
+					email,
+					api_key,
+				};
+			} catch (err) {
+				return err;
+			}
+		},
+	});
+
+	fastify.route({
+		method: 'POST',
+		url: '/getapikey',
+		schema: {
+			body: {
+				type: 'object',
+				required: ['email', 'password'],
+				properties: {
+					email: { type: 'string', format: 'email' },
+					password: { type: 'string', minLength: 8 },
+				},
+			},
+		},
+		handler: async (req, reply) => {
+			try {
+				const { email, password } = req.body;
+
+				const User = await fastify.db.getRegisteredUser(email);
+
+				if (User === false) {
+					reply.status(409);
+					return 'User not registered';
+				}
+
+				console.log(User)
+				if (await bcrypt.compare(password, User.password)) {
+					return User.api_key;
+				} else {
+					reply.status(403);
+					return 'Wrong password';
+				}
+			} catch (err) {
+				reply.status(500);
+				return err;
+			}
+		},
+	});
+};
+
+const create_UUID = () => {
+	var dt = new Date().getTime();
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+		/[xy]/g,
+		function (c) {
+			var r = (dt + Math.random() * 16) % 16 | 0;
+			dt = Math.floor(dt / 16);
+			return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+		},
+	);
+	return uuid;
 };
 
 export default apiRoutes;
--- apps/server/db.js
+++ apps/server/db.js
@@ -1,23 +1,37 @@
-import { createHash } from 'crypto';
-
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv';
+import {createHash} from 'crypto'
+dotenv.config()
 /**
  * @param {import('fastify').FastifyInstance} fastify
  */
 export const getDb = db => {
 	return {
-		/** @param {string} originalUrl */
-		async createShortUrl(originalUrl) {
-			// Shortening using MD5, will improve this in future
+		async verifyApiKey(api_key) {
+			try{
+			const doc = await db.collection('api_keys').findOne({ api_key });
+			if (doc === null) return false;
+			else return true;
+			}
+			catch(err){
+				return err;
+			}
+		},
+
+
+		async createShortUrl(originalUrl, api_key) {
+			
+			console.log(originalUrl+api_key+process.env.SECRET_KEY)
 			const shortCode = createHash('md5')
-				.update(originalUrl)
-				.digest('hex')
-				.slice(0, 6);
+			.update(originalUrl+api_key+process.env.SECRET_KEY)
+			.digest('hex')
+			.slice(0, 6);
 
 			await db
 				.collection('urls')
 				.updateOne(
 					{ _id: shortCode },
-					{ $set: { _id: shortCode, originalUrl, shortCode } },
+					{ $set: { _id: shortCode, originalUrl, shortCode , createdBy : api_key } },
 					{ upsert: true },
 				);
 			return shortCode;
@@ -28,6 +42,38 @@ export const getDb = db => {
 			/** @type {null|{originalUrl:string}} */
 			const doc = await db.collection('urls').findOne({ shortCode });
 			return doc?.originalUrl;
+		},
+
+		/** @param {string} email */
+		async getRegisteredUser(email) {
+			const doc = await db.collection('users').findOne({ email });
+			console.log('user', doc);
+			if (doc != null) {
+				return doc;
+			} else {
+				return false;
+			}
+		},
+
+		async createUser(email, password, api_key) {
+			try {
+				const res = await db.collection('users').insert({
+					_id: email,
+					email,
+					password,
+					api_key,
+				});
+
+				await db.collection('api_keys').insert({
+					_id: api_key,
+					api_key: api_key,
+				});
+
+				console.log('ss', res);
+				return res;
+			} catch (err) {
+				return err;
+			}
 		},
 	};
 };
