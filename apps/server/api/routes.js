89c6afe976ec8051179e1022200d3507ce1b2f1e
gagan <gagandeep20059@gmail.com>
2022-01-16 14:50:16 +0530

    merge

--- apps/server/api/routes.js
+++ apps/server/api/routes.js
@@ -7,6 +7,7 @@ const apiRoutes = async fastify => {
 		method: 'POST',
 		url: '/create',
 		schema: {
+			description: 'Create a new short URL',
 			body: {
 				type: 'object',
 				required: ['originalUrl', 'api_key'],
@@ -15,15 +16,23 @@ const apiRoutes = async fastify => {
 					api_key: { type: 'string' },
 				},
 			},
+			response: {
+				200: {
+					type: 'object',
+					properties: {
+						shortUrl: { type: 'string' },
+					},
+				},
+			},
 		},
-		handler: async (request,reply) => {
+		handler: async (request, reply) => {
 			let { originalUrl, api_key } = request.body;
 
 			if (await fastify.db.verifyApiKey(api_key)) {
 				let shortUrl = await fastify.db.createShortUrl(originalUrl, api_key);
 				return { shortUrl: `${request.hostname}/${shortUrl}` };
 			} else {
-				reply.status(403);
+				reply.status(401);
 				return 'Invalid Api key';
 			}
 		},
@@ -39,6 +48,21 @@ const apiRoutes = async fastify => {
 				properties: {
 					email: { type: 'string', format: 'email' },
 					password: { type: 'string', minLength: 8 },
+				},
+			},
+			response: {
+				200: {
+					description:
+						'An object containing user email and api_key indicating successful registration',
+					type: 'object',
+					properties: {
+						email: { type: 'string' },
+						api_key: { type: 'string' },
+					},
+					409: {
+						description: 'User already registered',
+						type: 'string',
+					},
 				},
 			},
 		},
@@ -81,6 +105,16 @@ const apiRoutes = async fastify => {
 					password: { type: 'string', minLength: 8 },
 				},
 			},
+			response: {
+				200: {
+					description: 'API key',
+					type: 'string',
+				},
+				401: {
+					description: 'Invalid Credentials',
+					type: 'string',
+				},
+			},
 		},
 		handler: async (req, reply) => {
 			try {
@@ -89,15 +123,15 @@ const apiRoutes = async fastify => {
 				const User = await fastify.db.getRegisteredUser(email);
 
 				if (User === false) {
-					reply.status(409);
+					reply.status(401);
 					return 'User not registered';
 				}
 
-				console.log(User)
+				console.log(User);
 				if (await bcrypt.compare(password, User.password)) {
 					return User.api_key;
 				} else {
-					reply.status(403);
+					reply.status(401);
 					return 'Wrong password';
 				}
 			} catch (err) {
