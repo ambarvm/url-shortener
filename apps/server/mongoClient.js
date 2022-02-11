447bf0ac8ee8fc87ea7be85109759903a9ce2bb9
Gagan <gagans@iitbhilai.ac.in>
2022-02-11 17:03:17 +0530

    production ready

--- apps/server/mongoClient.js
+++ apps/server/mongoClient.js
@@ -1,11 +1,14 @@
 import { MongoClient } from 'mongodb';
-
+import fs from 'fs'
 /** @type {MongoClient} */
 let client;
 
 async function connect() {
+	const rdspem= fs.readFileSync('rds.pem');
 	if (!client) {
-		client = await MongoClient.connect(process.env.MONGO_URL);
+		client = await MongoClient.connect(process.env.MONGO_URL,{
+			tlsCAFile: `rds.pem`
+		});
 	}
 	return client;
 }
